c4b08bc0a53c98bec4e5d3448fda267ea5b2d089
kayzer24 <dante2410@hotmail.fr>
2022-03-18 14:08:13 +0100

    Api platform annotation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,44 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email())
+//                ->setRoles(['ROLE_USER'])
+                ->setPassword($this->passwordHasher->hashPassword($user, 'password'))
+//                ->setUpdatedAt(new DateTimeImmutable())
+            ;
+
+            $manager->persist($user);
+
+            for ($a = 0; $a < rand(5, 15); $a++) {
+                $article = (new Article())
+                    ->setName($faker->text(50))
+                    ->setContent($faker->text(300))
+                    ->setAuthor($user)
+                    ;
+
+                $manager->persist($article);
+            }
+        }
 
         $manager->flush();
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -14,6 +15,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use ResourceId;
